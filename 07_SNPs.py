8e720ea98d726f7c50c9947732f9a0310902d443
Alexa Badalamenti <abadalamenti1@users.noreply.github.com>
2016-07-18 23:00:03 -0500

    Update 07_SNPs.py

--- 07_SNPs.py
+++ 07_SNPs.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python
-'''Working to convert the files into the proper format for Matrix eQTL
-
-1) SNP file
 id    Sam_01      Sam_02      Sam_03
 Snp_01        2       0       2
 
@@ -25,26 +21,28 @@ outfile1 = sys.argv[2]
 outfile2 = sys.argv[3]
 
 
-outsnps = gzip.open(outfile1, "wb")
-outloc = gzip.open(outfile2, "wb")
+outsnps = open(outfile1, "wb")
+outloc = open(outfile2, "wb")
 
 ##create header for SNP Location file
-head2 = "snp    chr     pos"
+head2 = "snp\tchr\tpos"
 outloc.write(head2 + "\n")
 
 ##create header for SNP file
 mfile = gzip.open(mergedfile, "r")
 header = mfile.readline()
 splitheader = header.split()
-splitheader = str(splitheader[8:])
+splitheader = str(splitheader[9:])
 splitheader = splitheader.replace("'", "")
-splitheader = splitheader.replace(",", "")
 splitheader = splitheader.replace("]", "")
-head = "id " + str(splitheader[9:])
+splitheader = splitheader.replace("[", "")
+splitheader = splitheader.replace(" ", "")
+splitheader = splitheader.replace(",", "\t")
+head = "id\t" + splitheader
 outsnps.write(head + "\n")
 
-##filter out comment lines using a list comprehension
-##loop to write out to files
+#filter out comment lines using a list comprehension
+#loop to write out to files
 impdatalines = [l for l in mfile if not l.startswith('#')]
 for line in impdatalines:
     arr = line.strip().split()
@@ -53,12 +51,13 @@ for line in impdatalines:
     dosagerow = str(dosagerow).replace("[", "")
     dosagerow = dosagerow.replace("]", "")
     dosagerow = dosagerow.replace("'", "")
-    dosagerow = dosagerow.replace(",", "")
+    dosagerow = dosagerow.replace(" ", "")
+    dosagerow = dosagerow.replace(",", "\t")
     rsid = arr[2]
     chr = arr[0]
     pos = arr[1]
-    outsnps.write(rsid + " " + str(dosagerow) + "\n")
-    outloc.write(rsid + " chr" + chr + "  " + pos + "\n")
+    outsnps.write(rsid+"\t"+dosagerow+"\n")
+    outloc.write(rsid+"\tchr"+chr+"\t"+pos+"\n")
 
 outsnps.close()
 outloc.close()
