20d1ceac1aaa0a091e05f3f4b027b508630c0db6
Alexa Badalamenti <abadalamenti1@users.noreply.github.com>
2017-03-27 12:31:38 -0500

    Update 13_R_Density_Plot.R

--- 13_R_Density_Plot.R
+++ 13_R_Density_Plot.R
@@ -17,13 +17,5 @@ YRIALLR <- dplyr::mutate(YRIALLR, R = sqrt(R2))
 POP_R <-dplyr::select(YRIALLR, POP, R)
 POP_R <-dplyr::filter(POP_R, !is.na(R))
 
-CHB.YRI <-dplyr::filter(POP_R, POP == 'CHB')
-GIH.YRI <-dplyr::filter(POP_R, POP == 'GIH')
-JPT.YRI <-dplyr::filter(POP_R, POP == 'JPT')
-LWK.YRI <-dplyr::filter(POP_R, POP == 'LWK')
-MEX.YRI <-dplyr::filter(POP_R, POP == 'MEX')
-MKK.YRI <-dplyr::filter(POP_R, POP == 'MKK')
-YRI.YRI <-dplyr::filter(POP_R, POP == 'YRI')
-
 #Plot predictive model
 ggplot(POP_R, aes(R, colour = POP)) + geom_density() + xlim(-1, 1)
