06ce0e0a16c26bfbbc29107151b7171e363a7e2a
Alexa Badalamenti <abadalamenti1@users.noreply.github.com>
2017-06-06 17:07:22 -0500

    Update 01_update_bed_file_liftover.R

--- 01_update_bed_file_liftover.R
+++ 01_update_bed_file_liftover.R
@@ -2,8 +2,8 @@
 #load dplyr
 library("dplyr")
 
-#read in .bim file from directory
-bim_file <- read.table("/home/aly/LWK/Original_Genotype_Data/hapmap3_r2_b36_fwd.consensus.qc.poly.bim", sep = "\t$
+#read in .bim file
+bim_file <- read.table("/home/aly/LWK/Original_Genotype_Data/hapmap3_r2_b36_fwd.consensus.qc.poly.bim", sep = "\t")
 
 #new order needs to be chr, position, position+1, rsid
 new_bim <- dplyr::select(bim_file, V1, V4)
@@ -13,5 +13,4 @@ new_bim <- dplyr::mutate(new_bim, rsid$V2)
 new_bim$V1 <- sub("^", "chr", new_bim$V1)
 
 #write out new .bed
-write.table(new_bim, '/home/aly/LWK/liftOver/LWK_liftOver.bed', quote = FALSE, sep = "\t", row.names = FALSE, col$
-
+write.table(new_bim, '/home/aly/LWK/liftOver/LWK_liftOver.bed', quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
