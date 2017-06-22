69ec1ce63d71c3e48fdea13440f88a930abeceb2
Alexa Badalamenti <abadalamenti1@users.noreply.github.com>
2017-06-21 21:03:00 -0500

    Add files via upload

---
+++ CHB_generate_db_files.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./generate_sqlite_dbs.py --results_sub_folder /home/aly/ElasticNet/CHB/ --betas_sub_folder /home/aly/ElasticNet/CHB/ --betas_include_clause _merged_weights --results_include_clause _merged_expression --output_folder Database_Files
+
